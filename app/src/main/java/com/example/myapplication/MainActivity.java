a3694a43e74f22558ae2d3a47512168e1f2259e2
tahakoutbi <mehdichbanimehdi@gmail.com>
2023-01-05 19:48:25 +0100

    Initial Commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,33 +7,57 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button1;
-    private String edit_text;
-    private Button button2;
+    private ImageButton button1;
+    private ImageButton button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onImageButtonClick(View view) {
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Add code to open the new activity here
-                EditText inputField = (EditText) findViewById(R.id.edit_text3);
-                String input = inputField.getText().toString();
-            }
-        });
+
+
+        Intent intent = getIntent();
+        String value = intent.getStringExtra("key");
+        int somme = Integer.parseInt(value);
+
+        button1 = findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Add code to open the new activity here
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                Intent intent = new Intent(MainActivity.this, Check.class);
                 startActivity(intent);
             }
         });
+        button2 = findViewById(R.id.button2);
+        EditText editText1 = (EditText) findViewById(R.id.editchallenge1);
+        EditText editText2 = (EditText) findViewById(R.id.editchallenge2);
+        String input1 = editText1.getText().toString();
+        String input2 = editText2.getText().toString();
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Add code to open the new activity here
+
+                Intent intent = new Intent(MainActivity.this, Check.class);
+                intent.putExtra("key1", input1);
+                intent.putExtra("key2", input2);
+                startActivity(intent);
+            }
+        });
+
     }
+
+    /*public void onImageButtonClick(View view) {
+
+    }
+    public void onChallenge(View view){
+
+    }
+    public void onCall(View view){
+
+    }*/
 }
